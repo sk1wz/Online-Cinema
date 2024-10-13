85f602ccb6847ecebb8d9fb9cdf0410b40eb1b6b
sk1wz <78929376+sk1wz@users.noreply.github.com>
2024-10-13 12:10:36 +0300

    Update useFilmsStore.ts

--- src/store/useFilmsStore.ts
+++ src/store/useFilmsStore.ts
@@ -1,1 +1,13 @@
 
+// Хранилище
+import { create } from 'zustand';
+
+interface PostState {
+    films: IFilm[];
+    setFilms: (newFilms: IFilm[]) => void;
+}
+
+export const usePostStore = create<PostState>((set) => ({
+    films: [],
+    setFilms: (newFilms: Ifilm[]) => set(() => ({ films: newFilms })),
+}));
